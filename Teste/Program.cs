502e91bd7e9374b138c6f950a82ad9629e60a842
Sthefany <s532401ed@gmail.com>
2023-05-30 15:02:48 -0400

    Inicio do ṕrojeto de divisão de dois número

---
+++ Teste/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("DIVISÃO");
+Console.Write("Informe o primeiro valor: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Informe o segundo valor: ");
+int b = int.Parse(Console.ReadLine());
+
+int result = a / b;
+Console.WriteLine($"\nResultado = {result}");
