88e8cb6cd25fbef074f8159d872ee963726b816c
Sthefany <s532401ed@gmail.com>
2023-05-30 15:27:52 -0400

    Segundo commit

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -1,8 +1,19 @@
-﻿Console.WriteLine("DIVISÃO");
-Console.Write("Informe o primeiro valor: ");
-int a = int.Parse(Console.ReadLine());
-Console.Write("Informe o segundo valor: ");
-int b = int.Parse(Console.ReadLine());
+﻿try
+{
+    Console.WriteLine("DIVISÃO");
+    Console.Write("Informe o primeiro valor: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Informe o segundo valor: ");
+    int b = int.Parse(Console.ReadLine());
 
-int result = a / b;
-Console.WriteLine($"\nResultado = {result}");
+    int result = a / b;
+    Console.WriteLine($"\nResultado = {result}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Erro!");
+}
+finally
+{
+    Console.WriteLine("\nObrigada por usar meu programa! XD");
+}
