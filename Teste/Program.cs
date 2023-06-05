877012619b302e20c64413d7bf68d81072289ad0
Sthefany <s532401ed@gmail.com>
2023-06-05 16:57:53 -0400

    Quarto Commit

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -9,6 +9,14 @@
     int result = a / b;
     Console.WriteLine($"\nResultado = {result}");
 }
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine($"\nNão é permitido divisão por zero! {ex.Message}");
+}
+catch(InvalidCastException ex)
+{
+    Console.WriteLine($"\nConversão de string para letra não é permitida! {ex.Message}");
+}
 catch(Exception ex)
 {
     Console.WriteLine($"\nErro! {ex.Message}");
